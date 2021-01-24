2ab7c2a866050d3f40d3500234bc81df22874c40
night <night@git.cu>
2021-01-23 21:44:44 -0500

    Añadidas las siguientes funciones: - Anadir Trabajador - Editar Trabajador - Borrar Trabajador

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\AddUserType;
+use App\Form\EditUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -60,8 +61,28 @@ class UsersController extends AbstractController
 	/**
 	 * @Route("/edit/{id}", name="app_users_edit")
 	 */
-	public function userEditAction($id = null)
+	public function userEditAction($id = null, Request $request, UserPasswordEncoderInterface $passwordEncoder)
 	{
-	    return $this->render('home/edit.html.twig');
+		if ( $id !== null) {
+			$em = $this->getDoctrine()->getManager();
+			$user = $em->getRepository(User::class)->find($id);
+		}else{
+			return $this->redirectToRoute('app_users');
+		}
+
+		if (!$user) {
+			return $this->redirectToRoute('app_users');
+		}
+
+		$form = $this->createForm(EditUserType::class, $user);
+		$form->handleRequest($request);
+		if ($form->isSubmitted() && $form->isValid()) {
+			$user->setPassword($passwordEncoder->encodePassword($user, '12345'));
+			$em->persist($user);
+			$em->flush();
+			return $this->redirectToRoute('app_users');
+		}
+
+		return $this->render('home/edit.html.twig', ['form' => $form->createView()]);
 	}
 }
